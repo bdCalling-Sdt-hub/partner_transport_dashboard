5541352832feabb8ac75e87986d5ffcee501704f
shukumar-542 <shukumar542@gmail.com>
2024-11-23 12:58:46 +0600

    Implemetn admin task table

---
+++ src/Components/AdminTaskTable/AdminTaskTable.jsx
@@ -0,0 +1,67 @@
+import { Button, Table } from 'antd'
+import React from 'react'
+const AdminTaskTable = ({dataSource }) => {
+    const columns = [
+        {
+          title: "SL no.",
+          dataIndex: "slNo",
+          key: "slNo",
+          width: "10%",
+        },
+        {
+          title: "Task",
+          dataIndex: "task",
+          key: "task",
+          width: "40%",
+        },
+        {
+          title: "Assigned Admin",
+          dataIndex: "admin",
+          key: "admin",
+          width: "30%",
+          render: (admin) => (
+            <div className="flex items-center">
+              <img src={admin.avatar} size="large" style={{ marginRight: 10 }} />
+              {admin.name}
+            </div>
+          ),
+        },
+        {
+          title: "Status",
+          dataIndex: "status",
+          key: "status",
+          width: "20%",
+          render: (status) => (
+            <Button
+              type="text"
+              style={{
+                color: "#31A24C",
+                backgroundColor: "#E6F4EA",
+                fontWeight: "bold",
+                borderRadius: "20px",
+              }}
+            >
+              {status}
+            </Button>
+          ),
+        },
+      ];
+    return (
+        <div style={{ padding: "20px" }}>
+            <Table
+                dataSource={dataSource}
+                columns={columns}
+                pagination={false}
+                // pagination={{
+                //     total: 1239,
+                //     pageSize: 11,
+                //     showSizeChanger: false,
+                //     showQuickJumper: true,
+                // }}
+                bordered={false}
+            />
+        </div>
+    )
+}
+
+export default AdminTaskTable
--- src/Pages/SupervisionDashboard/SupervisionDashboard.jsx
+++ src/Pages/SupervisionDashboard/SupervisionDashboard.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { PieChart, Pie, Cell, Legend } from "recharts";
+import AdminTaskTable from '../../Components/AdminTaskTable/AdminTaskTable';
+import img from '../../assets/images/conver.png'
+
 const SupervisionDashboard = () => {
     const data = [
         { name: "Completed", value: 80 }, // Adjust values as needed
@@ -8,6 +11,49 @@ const SupervisionDashboard = () => {
 
     // Colors for the chart
     const COLORS = ["#0088FE", "#FFBB28"];
+
+    const dataSource = [
+        {
+          key: "1",
+          slNo: "#12333",
+          task: "Reviewing user payment dispute",
+          admin: {
+            name: "Jacob Jones",
+            avatar: img,
+          },
+          status: "Resolved",
+        },
+        {
+          key: "2",
+          slNo: "#12333",
+          task: "User management activity",
+          admin: {
+            name: "Darlene Robertson",
+            avatar: img,
+          },
+          status: "Resolved",
+        },
+        {
+          key: "3",
+          slNo: "#12333",
+          task: "User management activity",
+          admin: {
+            name: "Brooklyn Simmons",
+            avatar:img,
+          },
+          status: "Resolved",
+        },
+        {
+          key: "3",
+          slNo: "#12333",
+          task: "User management activity",
+          admin: {
+            name: "Brooklyn Simmons",
+            avatar:img,
+          },
+          status: "Resolved",
+        }
+    ]
     return (
         <div className=' p-4 rounded-md'>
             <div className='flex items-center justify-between gap-10'>
@@ -29,11 +75,12 @@ const SupervisionDashboard = () => {
                 </div>
             </div>
 
-            <div className='grid grid-cols-12 gap-2'>
-                <div className='col-span-8'>
-
+            <div className='grid grid-cols-12 gap-5 mt-5'>
+                <div className='col-span-8 bg-white rounded-md '>
+                    <div className='mt-2 ml-2 text-2xl font-medium'>Task Completed</div>
+                    <AdminTaskTable dataSource={dataSource}  />
                 </div>
-                <div className='bg-white mt-5 col-span-4 rounded-md ' style={{ textAlign: "center" }}>
+                <div className='bg-white  col-span-4 rounded-md flex flex-col items-center justify-center ' style={{ textAlign: "center" }}>
                     <h3 className='text-2xl font-semibold py-2'>Task Completion Rate</h3>
                     <PieChart width={200} height={200} className='mx-auto'>
                         {/* Create the Pie */}
@@ -54,6 +101,13 @@ const SupervisionDashboard = () => {
                         {/* Add a Legend */}
 
                     </PieChart>
+                    <div className='flex items-center justify-center '>
+                        <span className='bg-[#0088FE]  h-3 w-3 mr-1'></span>
+                        <p>Complete</p>
+
+                        <span className='bg-[#FFBB28]  h-3 w-3 ml-10 mr-1'></span>
+                        <p><span className=''></span>Ongoing</p>
+                    </div>
                 </div>
             </div>
 
