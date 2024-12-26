0d3722c75be7351b3b7e48b460586c96686f1e3f
shukumar-542 <shukumar542@gmail.com>
2024-12-26 14:10:23 +0600

    update 3.4

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server :{
+//     // host: "192.168.10.26",
+//     // port: "3002",
+//     host: "157.245.94.100",
+//     port: "3001",
+//   }
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 export default defineConfig({
   plugins: [react()],
-  server :{
-    // host: "192.168.10.26",
-    // port: "3002",
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/Components'),
+    },
+  },
+  server: {
     host: "157.245.94.100",
     port: "3001",
-  }
-})
+    //  host: "192.168.10.26",
+    // port: "3002",
+  },
+});
